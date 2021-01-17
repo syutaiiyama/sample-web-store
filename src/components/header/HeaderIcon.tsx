d094fadbdfc6ac35d4f2a1f48066e1e3606cf459
Iiyama <s.iiyama@antyba.com>
2021-01-17 18:31:57 +0900

    [MOD] ヘッダーアイコンをボタン化

--- src/components/header/HeaderIcon.tsx
+++ src/components/header/HeaderIcon.tsx
@@ -1,40 +1,53 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Pages } from "../../interfaces/pages";
 import MenuBookIcon from "@material-ui/icons/MenuBook";
 import WorkSharpIcon from "@material-ui/icons/WorkSharp";
 import FastfoodOutlinedIcon from "@material-ui/icons/FastfoodOutlined";
-import { ListItemIcon } from "@material-ui/core";
+import { IconButton } from "@material-ui/core";
+import { useRouter } from "next/router";
 
 type HeaderIconProps = {
   pageType: string;
 };
 
 export const HeaderIcon: React.FC<HeaderIconProps> = ({ pageType }) => {
+  const router = useRouter();
+  const handleClick = useCallback((url: string) => {
+    router.push(url);
+  }, []);
+
   if (pageType === Pages.Book)
     return (
-      <MenuBookIcon
-        fontSize={"large"}
-        color={"primary"}
-        style={{ margin: "0 30px" }}
-      />
+      <IconButton onClick={() => handleClick(Pages.Book)}>
+        <MenuBookIcon
+          fontSize={"large"}
+          color={"primary"}
+          style={{ margin: "0 10px" }}
+        />
+      </IconButton>
     );
 
   if (pageType === Pages.Clothe)
     return (
-      <WorkSharpIcon
-        fontSize={"large"}
-        color={"primary"}
-        style={{ margin: "0 30px" }}
-      />
+      <IconButton onClick={() => handleClick(Pages.Clothe)}>
+        <WorkSharpIcon
+          fontSize={"large"}
+          color={"primary"}
+          style={{ margin: "0 10px" }}
+        />
+      </IconButton>
     );
 
   if (pageType === Pages.Food)
     return (
-      <FastfoodOutlinedIcon
-        fontSize={"large"}
-        color={"primary"}
-        style={{ margin: "0 30px" }}
-      />
+      <IconButton onClick={() => handleClick(Pages.Food)}>
+        <FastfoodOutlinedIcon
+          fontSize={"large"}
+          color={"primary"}
+          style={{ margin: "0 10px" }}
+          onClick={() => handleClick(Pages.Food)}
+        />
+      </IconButton>
     );
 
   return <></>;
