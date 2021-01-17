0534703574cb2a76b9c6b2e7e0230617a84a3d7a
Iiyama <s.iiyama@antyba.com>
2021-01-17 10:39:44 +0900

    [MOD] 環境周り変更

---
+++ next.config.js
@@ -0,0 +1,27 @@
+const { withPlugins } = require("next-compose-plugins");
+const withOptimizedImages = require("next-optimized-images");
+const envStaging = require("./env/staging");
+const envLocal = require("./env/local");
+const envProduction = require("./env/production");
+
+const env =
+  process.env.NODE_ENV === "production"
+    ? envProduction
+    : process.env.API_ENV === "local"
+    ? envLocal
+    : envStaging;
+
+// next.js configuration
+const nextConfig = {
+  env,
+  webpack: (config) => {
+    config.resolve.modules.push(__dirname);
+
+    config.resolve.alias = {
+      ...config.resolve.alias,
+    };
+    return config;
+  },
+};
+
+module.exports = withPlugins([withOptimizedImages], nextConfig);
